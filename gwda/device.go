9d6a57bb850ba0c6048d80552ff2737275e1f02c
ranxx <ran.star@foxmail.com>
2024-07-28 19:32:38 +0800

    gwda

---
+++ gwda/device.go
@@ -0,0 +1,52 @@
+package gwda
+
+import (
+	"fmt"
+
+	giDevice "github.com/ranxx/electricbubble/gidevice"
+)
+
+type Device struct {
+	deviceID     int
+	serialNumber string
+	Port         int
+	MjpegPort    int
+
+	d giDevice.Device
+}
+
+func DeviceList() (devices []Device, err error) {
+	var usbmux giDevice.Usbmux
+	if usbmux, err = giDevice.NewUsbmux(); err != nil {
+		return nil, fmt.Errorf("usbmuxd: %w", err)
+	}
+
+	var deviceList []giDevice.Device
+	if deviceList, err = usbmux.Devices(); err != nil {
+		return nil, fmt.Errorf("device list: %w", err)
+	}
+
+	devices = make([]Device, len(deviceList))
+
+	for i := range devices {
+		devices[i].deviceID = deviceList[i].Properties().DeviceID
+		devices[i].serialNumber = deviceList[i].Properties().SerialNumber
+		devices[i].Port = 8100
+		devices[i].MjpegPort = 9100
+		devices[i].d = deviceList[i]
+	}
+
+	return
+}
+
+func (d Device) DeviceID() int {
+	return d.deviceID
+}
+
+func (d Device) SerialNumber() string {
+	return d.serialNumber
+}
+
+func (d Device) GIDevice() giDevice.Device {
+	return d.d
+}
